b9fe5da6780c0c85ce30f6334781c498f76e73df
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-09-05 12:43:01 -0500

    Styling contact page

--- landing/src/page/pages/Contact.jsx
+++ landing/src/page/pages/Contact.jsx
@@ -1,3 +1,9 @@
-export const Contact = ()=>{return(<>
-<p>Contact  🤦  </p>
-</>)}
+export const Contact = () => {
+    return (<>
+        <div className="flex flex-row justify-around items-center mt-10">
+            <div>          <Logo className="object-cover h-48 w-96" />
+            </div>
+            <div><Form /> </div>
+        </div>
+    </>)
+}
