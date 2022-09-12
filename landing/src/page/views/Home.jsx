915c48e1518319463eb0ea5d75a5375e6f72d7f0
Karol <kypacheco07@misena.edu.co>
2022-09-12 00:26:53 -0500

    Creating home view

--- landing/src/page/views/Home.jsx
+++ landing/src/page/views/Home.jsx
@@ -1,14 +1,32 @@
-import { Button, Typography } from "@mui/material";
+import { Grid, Box, Paper, Typography } from "@mui/material";
+import BasicCard from "../components/HomeCard";
 
 export const Home = () => {
   return (
-    <div className="mt-10">
+    <>
       <Typography sx={{ ml: 40 }} color="primary" variant="h1">
         Home
       </Typography>
-      <Button sx={{ ml: 40 }} large variant="contained" color="secondary">
-        Home
-      </Button>
-    </div>
+      <Grid sx={{ m: 30, ml: 40 }} container justify="between">
+        <Grid item>
+          <BasicCard />
+        </Grid>
+        <Grid item>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              "& > :not(style)": {
+                m: 1,
+                width: 128,
+                height: 128,
+              },
+            }}
+          >
+            <Paper color="warning" elevation={3} />
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 };
