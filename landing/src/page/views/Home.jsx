dffe0f642eee2fa79b45eab67ad10410bf81e1bd
Karol <kypacheco07@misena.edu.co>
2022-09-14 18:35:17 -0500

    Creating home view

--- landing/src/page/views/Home.jsx
+++ landing/src/page/views/Home.jsx
@@ -1,32 +1,61 @@
-import { Grid, Box, Paper, Typography } from "@mui/material";
-import BasicCard from "../components/HomeCard";
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  Box,
+  Avatar,
+} from "@mui/material";
+import "animate.css";
 
 export const Home = () => {
   return (
-    <>
-      <Typography sx={{ ml: 40 }} color="primary" variant="h1">
-        Home
-      </Typography>
-      <Grid sx={{ m: 30, ml: 40 }} container justify="between">
-        <Grid item>
-          <BasicCard />
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        p: 10,
+        ml: { sm: "30vh" },
+      }}
+    >
+      <Grid
+        display="flex"
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        columns={16}
+      >
+        <Grid sx={{ mt: 6 }} item xs={12} md={8}>
+          <Card sx={{ height: "60vh" }}>
+            <CardContent align="justify">
+              <Typography
+                sx={{ mt: 6 }}
+                color="secondary.main"
+                gutterBottom
+                variant="h4"
+                component="div"
+              >
+                Let's dive in !
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                This is my landing page.
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                I am a developer student focused in Front-end.
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item>
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              "& > :not(style)": {
-                m: 1,
-                width: 128,
-                height: 128,
-              },
-            }}
-          >
-            <Paper color="warning" elevation={3} />
-          </Box>
+        <Grid sx={{ mt: 6 }} item xs={12} md={6}>
+          <Avatar
+            className="animate__slow animate__animated animate__bounce"
+            sx={{ width: "56vh", height: "56vh" }}
+            alt="avatar"
+            src="/avatarlogo.svg"
+          />
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };
