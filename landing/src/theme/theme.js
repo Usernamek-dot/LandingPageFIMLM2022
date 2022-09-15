4c6f190dc00f0aa8776b98ddfabe930dd50ff379
Karol <kypacheco07@misena.edu.co>
2022-09-14 22:42:59 -0500

    Adding new palette

--- landing/src/theme/theme.js
+++ landing/src/theme/theme.js
@@ -2,9 +2,24 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     mode: "dark",
-    primary: { main: "#33313B" },
-    secondary: { main: "#E3C4A8  " },
-    tertiary: { main: "#f6f5f5" },
+
+    // background
+    primary: {
+      main: "#181A1B",
+      light: "#181A1B1A",
+      dark: "#09090C",
+      contrast: "#0E0F15",
+    },
+    // text
+
+    secondary: {
+      main: "#E8E6E3",
+      light: "#BDB8AF",
+      dark: "#52A6FF",
+      contrast: "#9E9689",
+    },
+    //borders
+    tertiary: { main: "#7E7567" },
   },
   typography: { fontFamily: `'Poppins', sans-serif` },
 });
