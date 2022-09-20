9fd89d2d7065e8dc2dbe5684dd408a5476625cc8
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-09-20 10:43:46 -0500

    Feat - added image of not found page

--- landing/src/auth/NotFound.jsx
+++ landing/src/auth/NotFound.jsx
@@ -1,3 +1,31 @@
+import { Grid, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div>Error404</div>;
+  // const navigate = useNavigate();
+  // const goBack = () => {
+  //   navigate(-1);
+  // };
+  // onClick={goBack}
+  return;
+  <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ minHeight: "100vh" }}
+  >
+    <Grid sx={{ m: 4 }} item md={6} xs={12}>
+      <Typography variant="h5" gutterBottom>
+        Sorry, could not find it for you.
+      </Typography>
+      {/* <Button uppercase variant="outlined">
+          Go Back
+        </Button> */}
+    </Grid>
+    <Grid item md={6} xs={12}>
+      <img height="200" src="./errorpage.svg" alt="404 not found" />
+    </Grid>
+  </Grid>;
 };
